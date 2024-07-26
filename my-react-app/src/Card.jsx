7277a9a357300cd5b0d16caf06bd9b5540da87e5
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-26 15:21:19 +0530

    card components

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <img className="card-image" src="https://via.placeholder.com/150"/>
+      <h2 className="card-title">Vasu Code</h2>
+      <p className="card-text">I like watching movies</p>
+    </div>
+  )
+}
+
+export default Card
