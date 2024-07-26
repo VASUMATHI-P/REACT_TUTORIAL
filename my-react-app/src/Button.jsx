33423a4fb034dbfe8f16426af8ea97205348aa13
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-26 17:01:19 +0530

    add inline css

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,14 @@
+const Button = () => {
+  const styles = {
+    backgroundColor: "blue",
+    color: "white",
+    border: "none",
+    padding: "10px",
+    borderRadius: "10%"
+  }
+  return (
+    <button style= {styles}>Click me !</button>
+  )
+}
+
+export default Button
