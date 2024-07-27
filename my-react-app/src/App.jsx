089c570f3cd5810ee4130be28966345883ba87b0
VASUMATHI-P <vasumathi.p2021csec@sece.ac.in>
2024-07-27 10:45:25 +0530

    render lists using props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,25 @@
 import List from "./List"
 
 function App() {
+  const fruits = [
+    { id: 1, name: 'Apple', calories: 90},
+    { id: 2, name: 'Banana', calories: 105},
+    { id: 3, name: 'Mango', calories: 100},
+    { id: 4, name: 'Orange', calories: 45}
+  ];
+
+  const vegetables = [
+    { id: 1, name: 'Carrot', calories: 41 },
+    { id: 2, name: 'Broccoli', calories: 55 },
+    { id: 3, name: 'Spinach', calories: 23 },
+    { id: 4, name: 'Potato', calories: 77 }
+  ];
+
   return (
     <>
-     <List/>
+      {fruits.length > 0 && <List items= {fruits} category= "Fruits"/>}
+      {vegetables.length > 0 && <List items= {vegetables} category= "Vegetables"/>}
+      <List/>
     </>
   )
 
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,54 +1,35 @@
-const List = () => {
-  const fruits = [
-    {
-      id: 1,
-      name: 'Apple',
-      calories: 90
-    },
-    {
-      id: 2,
-      name: 'Banana',
-      calories: 105
-    },
-    {
-      id: 3,
-      name: 'Mango',
-      calories: 100
-    },
-    {
-      id: 4,
-      name: 'Orange',
-      calories: 45
-    }
-  ];
+import PropTypes from 'prop-types';
 
-  fruits.sort((a, b) => a.name.localeCompare(b.name))  //ALPHABETICAL
-  fruits.sort((a, b) => b.name.localeCompare(a.name))  //EVERSE ALPHABETIC
-  fruits.sort((a, b) => a.calories - b.calories)  //NUMERIC
-  fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
+const List = (props) => {
+  const items = props.items;
+  const category = props.category;
+  // fruits.sort((a, b) => a.name.localeCompare(b.name))  //ALPHABETICAL
+  // fruits.sort((a, b) => b.name.localeCompare(a.name))  //EVERSE ALPHABETIC
+  // fruits.sort((a, b) => a.calories - b.calories)  //NUMERIC
+  // fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
 
-  const lowCalories = fruits.filter(fruit => fruit.calories < 100)
-
-  const lowCaloriesList = lowCalories.map(fruit => <li key={fruit.id}>
-    {fruit.name}: &nbsp; <b>{fruit.calories}</b>
-    </li>)
-
-  const highCalories = fruits.filter(fruit => fruit.calories >= 100)
-
-  const highCaloriesList = highCalories.map(fruit => <li key={fruit.id}>
-    {fruit.name}: &nbsp; <b>{fruit.calories}</b>
-    </li>)
-
-  const fruitsList = fruits.map(fruit => <li key={fruit.id}>
-                                          {fruit.name}: &nbsp; <b>{fruit.calories}</b>
+  const itemsList = items.map(item => <li key={item.id}>
+                                          {item.name}: &nbsp; <b>{item.calories}</b>
                                           </li>)
   return (
-    <div>
-      <ul>{fruitsList}</ul>
-      <ul>{lowCaloriesList}</ul>
-      <ul>{highCaloriesList}</ul>
-    </div>
+    <>
+      <h2 className="list-category">{category}</h2>
+      <ul className="list-items">{itemsList}</ul>
+    </>
   )
+}
+
+List.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    calories: PropTypes.number
+  }))
+}
+
+List.defaultProps = {
+  items: [],
+  category: 'Category'
 }
 
 export default List
